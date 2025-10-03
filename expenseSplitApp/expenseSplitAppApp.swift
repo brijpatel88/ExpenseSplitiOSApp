352837adc4636b6052591383335b6b51385db673
brijpatel88 <Brijeshkumar.Patel@triosstudent.com>
2025-10-03 17:48:03 -0400

    Initial Commit

---
+++ expenseSplitApp/expenseSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseSplitAppApp.swift
+//  expenseSplitApp
+//
+//  Created by Brijesh Patel on 2025-10-03.
+//
+
+import SwiftUI
+
+@main
+struct expenseSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expenseSplitAppTests/expenseSplitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  expenseSplitAppTests.swift
+//  expenseSplitAppTests
+//
+//  Created by Brijesh Patel on 2025-10-03.
+//
+
+import Testing
+@testable import expenseSplitApp
+
+struct expenseSplitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
